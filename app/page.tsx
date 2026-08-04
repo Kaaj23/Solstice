30f534e3097aa57c2d3f7bf021237eedc69e2211
Kaaj23 <99437107+Kaaj23@users.noreply.github.com>
2026-08-04 12:50:01 +0400

    page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+import Footer from '@/components/Footer';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#090a0f] text-gray-100 flex flex-col justify-between">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </main>
+  );
+}
