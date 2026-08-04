4b81f619a7735c0f6090c4190e32f3d332345209
Kaaj23 <99437107+Kaaj23@users.noreply.github.com>
2026-08-04 13:23:43 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import Footer from '@/components/Footer';
+"use client";
+
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
